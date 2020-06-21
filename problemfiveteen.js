895c453d922975780691135e267c49fe7ded7862
Mehmet Can Savaş <62428567+MehmetCanSavas@users.noreply.github.com>
2020-06-21 20:25:01 +0300

    Add files via upload

--- problemfiveteen.js
+++ problemfiveteen.js
@@ -1,1 +1,22 @@
+var numRoutes = [];
 
+var getNumRoutes = function(sizeX, sizeY) {
+    numRoutes[sizeX] = numRoutes[sizeX] || []
+
+    if (numRoutes[sizeX][sizeY]) {
+      return numRoutes[sizeX][sizeY];
+    }
+    if(sizeX == 0 || sizeY == 0) return 1;
+
+    //Consider moving to the right
+    var routes = getNumRoutes(sizeX - 1, sizeY);
+    routes += getNumRoutes(sizeX, sizeY - 1);
+
+    numRoutes[sizeX][sizeY] = routes;
+
+    return routes;
+};
+
+console.log(getNumRoutes(2, 2));
+console.log(getNumRoutes(20, 20));
+//javascript + Mootols 1.4.5(framework ve extension) ile çözüldü
